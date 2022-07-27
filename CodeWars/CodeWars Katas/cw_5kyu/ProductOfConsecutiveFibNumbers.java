76a9e4545752b4d441d96587400a5e6d3adb1e1c
Artem <artemkov1990@gmail.com>
2022-07-28 00:51:53 +0300

    Initial commit

---
+++ CodeWars/CodeWars Katas/cw_5kyu/ProductOfConsecutiveFibNumbers.java
@@ -0,0 +1,70 @@
+package cw_5kyu;
+
+/*
+The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+
+such as
+
+F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+
+Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+
+F(n) * F(n+1) = prod.
+
+Your function productFib takes an integer (prod) and returns an array:
+
+[F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+depending on the language if F(n) * F(n+1) = prod.
+
+If you don't find two consecutive F(n) verifying F(n) * F(n+1) = prodyou will return
+
+[F(n), F(n+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+F(n) being the smallest one such as F(n) * F(n+1) > prod.
+
+Some Examples of Return:
+(depend on the language)
+
+productFib(714) # should return (21, 34, true),
+                # since F(8) = 21, F(9) = 34 and 714 = 21 * 34
+
+productFib(800) # should return (34, 55, false),
+                # since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
+-----
+productFib(714) # should return [21, 34, true],
+productFib(800) # should return [34, 55, false],
+-----
+productFib(714) # should return {21, 34, 1},
+productFib(800) # should return {34, 55, 0},
+-----
+productFib(714) # should return {21, 34, true},
+productFib(800) # should return {34, 55, false},
+Note:
+You can see examples for your language in "Sample Tests".
+ */
+
+public class ProductOfConsecutiveFibNumbers {
+    static final long[] FIBONACHI = new long[]{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597,
+            2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269,
+            2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155, 165580141, 267914296,
+            433494437, 701408733, 1134903170, 1836311903, 2971215073L};
+    public static long[] productFib(long prod) {
+        System.out.println("prod : " + prod);
+        long[] result = new long[]{};
+        if (prod == 1) {
+            result = new long[]{1, 1, 1};
+        } else {
+            for (int i = 0; i < FIBONACHI.length -1; i++) {
+                if (FIBONACHI[i] * FIBONACHI[i+1] == prod) {
+                    result = new long[]{FIBONACHI[i], FIBONACHI[i+1], 1};
+                    break;
+                } else if (FIBONACHI[i] * FIBONACHI[i+1] > prod) {
+                    result = new long[]{FIBONACHI[i], FIBONACHI[i + 1], 0};
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ CodeWars/CodeWars Katas/cw_5kyu/Scramblies.java
@@ -0,0 +1,32 @@
+package cw_5kyu;
+
+/*
+Complete the function scramble(str1, str2) that returns true if a portion of str1 characters
+can be rearranged to match str2, otherwise returns false.
+
+Notes:
+
+Only lower case letters will be used (a-z). No punctuation or digits will be included.
+Performance needs to be considered.
+Examples
+scramble('rkqodlw', 'world') ==> True
+scramble('cedewaraaossoqqyt', 'codewars') ==> True
+scramble('katas', 'steak') ==> False
+ */
+
+public class Scramblies {
+
+    public static boolean scramble(String str1, String str2) {
+        if (str2.length() > str1.length()) {
+            return false;
+        } else {
+            for (String s : str2.split("")) {
+                if (!str1.contains(s)) {
+                    return false;
+                }
+                str1 = str1.replaceFirst(s, "");
+            }
+            return true;
+        }
+    }
+}
